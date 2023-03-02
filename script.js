0624fdfd94d70029b499a6a398428259ec67afa9
Aron <73133960+orcatortugs@users.noreply.github.com>
2023-03-02 08:14:16 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+ //Make Navbar Solid After Scrolling Down
  $(document).scroll(function() {
         $(".navbar").toggleClass(
           "scrolled",
           $(this).scrollTop() > $(".navbar").height()
         );
+      });
+
+      //Move to top left after scorlling
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) { // adjust the threshold as needed
+          $('.box').addClass('fixed');
+        } else {
+          $('.box').removeClass('fixed');
+        }
       });

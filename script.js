87f514f333946a14bf84d657cd4795b1c364ef9b
Aron <73133960+orcatortugs@users.noreply.github.com>
2023-03-05 14:52:58 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,36 @@
           $('.box-navbar').removeClass('show');
         }
       });
+$('#carouselFade').carousel();
+
+//Takes you to search 
+function searchToRoom() {
+  
+  // Get the selected room type
+  const roomType = $('#roomTypeHomepage').val();;
+  alert(roomType);
+  // Get the selected date
+  const date = document.querySelector(".input-field1-navbar").value;
+
+  // Store the data in localStorage
+  localStorage.setItem("roomTypeStored", roomType);
+  localStorage.setItem("date", date);
+
+  // Redirect to the booking page
+  window.location.href = "booking.html";
+
+  //Set selections
+  $("#roomTypeDropdown").val(roomType);
+}
+
+//Search for available rooms
+function search() {
+  const checkInDate = document.querySelector('input[type="date"]:nth-of-type(1)').value;
+  const checkOutDate = document.querySelector('input[type="date"]:nth-of-type(2)').value;
+  const roomType = document.querySelector('select:nth-of-type(1)').value;
+  const bedConfig = document.querySelector('select:nth-of-type(2)').value;
+  const numRooms = document.querySelector('input[type="number"]').value;
+  const kitchen = document.querySelector('input[type="checkbox"]').checked;
+
+  //Do something with the variables
+}
